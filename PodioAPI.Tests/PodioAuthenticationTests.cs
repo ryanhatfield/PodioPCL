faeaa9d989fa4842fc4b2211dc8dc63a6c8dd46a
Ryan Hatfield <ryan.hatfield@onsharp.com>
2014-12-10 03:11:37 -0600

    Added a unit test project to test the progress thus far. Getting a simple item list works well, starting to convert the Podio class into a more asyncronous API

---
+++ PodioAPI.Tests/PodioAuthenticationTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PodioAPI.Utils;
+using PodioAPI.Models;
+using PodioAPI.Utils.Authentication;
+
+namespace PodioAPI.Tests
+{
+	[TestClass]
+	public class PodioAuthenticationTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Podio _Podio;
+			StaticAuthStore _AuthStore;
+
+			_AuthStore = new StaticAuthStore();
+			_Podio = new Podio(Settings.ClientId, Settings.ClientSecret, _AuthStore);
+			_Podio.AuthenticateWithApp(Settings.AppId, Settings.AppToken);
+
+			var moments = _Podio.ItemService.FilterItems(Settings.AppId);
+
+			Assert.IsNotNull(moments);
+		}
+
+
+		public static class Settings
+		{
+			public static readonly string ClientId = "";
+			public static readonly string ClientSecret = "";
+
+			public static readonly int AppId = 0;
+			public static readonly string AppToken = "";
+
+		}
+	}
+}
