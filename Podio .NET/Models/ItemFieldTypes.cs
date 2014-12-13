cd30d8eff04d3c70ebdb4928806098097ca517c8
Ryan Hatfield <ryan@ryan.bio>
2014-12-13 03:32:19 -0600

    Moved some of the serialization classes (internal only) and added a ItemFieldConverter to convert a list of Podio.Item 's to their correct type upon de-serialization.
    Signed-off-by: Ryan Hatfield <ryan.hatfield@onsharp.com>

---
+++ Podio .NET/Models/ItemFieldTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodioAPI.Models
+{
+	internal enum ItemFieldTypes
+	{
+		Other,
+		Text,
+		Number,
+		State,
+		Image,
+		Date,
+		App,
+		Member,
+		Contact,
+		Money,
+		Progress,
+		Location,
+		Video,
+		Duration,
+		Embed,
+		Question,
+		Category,
+	}
+}
--- Podio .NET/Utils/Serialization/JSONSerializer.cs
+++ Podio .NET/Utils/Serialization/JSONSerializer.cs
@@ -1,51 +1,21 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+using PodioAPI.Utils.Serialization;
 
 namespace PodioAPI.Utils
 {
-    internal class JSONSerializer
-    {       
-        public static string Serilaize(object entity)
-        {
-            var settings = new JsonSerializerSettings();
-            settings.NullValueHandling = NullValueHandling.Ignore;
+	internal class JSONSerializer
+	{
+		public static string Serilaize(object entity)
+		{
+			var settings = new JsonSerializerSettings();
+			settings.NullValueHandling = NullValueHandling.Ignore;
 
-            return JsonConvert.SerializeObject(entity, settings);
-        }
+			return JsonConvert.SerializeObject(entity, settings);
+		}
 
-        public static T Deserilaize<T>(string json)
-        {
-            return JsonConvert.DeserializeObject<T>(json, new JsonConverter[] { new NestedDictionaryConverter() });
-        }
-    }
-
-    #region Nested Dictionary converter
-    internal class NestedDictionaryConverter : Newtonsoft.Json.Converters.CustomCreationConverter<IDictionary<string, object>>
-    {
-        public override IDictionary<string, object> Create(Type objectType)
-        {
-            return new Dictionary<string, object>();
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            // in addition to handling IDictionary<string, object>
-            // we want to handle the deserialization of dict value
-            // which is of type object
-            return objectType == typeof(object) || base.CanConvert(objectType);
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            if (reader.TokenType == JsonToken.StartObject
-                || reader.TokenType == JsonToken.Null)
-                return base.ReadJson(reader, objectType, existingValue, serializer);
-
-            // if the next token is not an object
-            // then fall back on standard deserializer (strings, numbers etc.)
-            return serializer.Deserialize(reader);
-        }
-    }
-    #endregion
+		public static T Deserilaize<T>(string json)
+		{
+			return JsonConvert.DeserializeObject<T>(json, new JsonConverter[] { new NestedDictionaryConverter() });
+		}
+	}
 }
